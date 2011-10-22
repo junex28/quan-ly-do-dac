f4eee9722898dee9a443bcdf7df104776502e3b7
minhtbv@gmail.com <minhtbv@gmail.com@d227f6ed-bcee-4286-e2fd-199b1918873a>
2011-10-22 11:56:28 +0000

    modify layout

--- trunk/implementation/QuanLyDoDac/QuanLyDoDac/Global.asax.cs
+++ trunk/implementation/QuanLyDoDac/QuanLyDoDac/Global.asax.cs
@@ -24,7 +24,7 @@ namespace QuanLyDoDac
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "TrangChu", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
